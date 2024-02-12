9b5044b58be010c891b03d29f6ced15181656577
Mahdi Niyati <niyat2163@gmail.com>
2024-02-12 18:57:08 +0330

    modal responsive

--- src/components/modules/Modal.jsx
+++ src/components/modules/Modal.jsx
@@ -18,15 +18,15 @@ const Modal = ({ modal, setModal, chart }) => {
   console.log(convertData(chart.data, type));
   chart;
   return (
-    <div className=" w-full h-full  fixed  bg-[#2E303D] bg-opacity-5 backdrop-blur-sm  z-50">
+    <div className=" w-full h-full flex flex-col items-center xl:block fixed top-0   bg-[#2E303D] bg-opacity-5 backdrop-blur-sm  z-50">
       <p
         onClick={() => setModal(null)}
-        className=" text-red-500 font-bold text-2xl ml-10 border-2 inline-block px-2 rounded-md cursor-pointer"
+        className=" text-red-500 mt-20 font-bold text-2xl xl:ml-10 border-2 inline-block px-2 rounded-md cursor-pointer"
       >
         X
       </p>
-      <div className="flex flex-col justify-center items-center   w-[800px]  m-auto p-20 bg-[#18181ce6] rounded-2xl mt-36 border-2 border-bor ">
-        <div className=" w-[760px] h-[300px]">
+      <div className="flex flex-col justify-center items-center w-[310px] mt-20 xl:mt-32  xl:w-[800px] xl:m-auto xl:p-20 bg-[#18181ce6] rounded-2xl xl:border-2 xl:border-bor ">
+        <div className=" w-[300px] h-[200px] xl:w-[760px] xl:h-[300px] ">
           <ResponsiveContainer width="100%" height="100%">
             <LineChart
               width={400}
@@ -37,7 +37,7 @@ const Modal = ({ modal, setModal, chart }) => {
                 type={"monotone"}
                 dataKey={type}
                 stroke="#00FFA3"
-                strokeWidth="2px"
+                strokeWidth="1px"
               />
               <CartesianGrid stroke="#404042" />
               <YAxis
